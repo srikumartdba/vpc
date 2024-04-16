7ec56ea00850a3eac55074341d705aff7764960c
srikumartdba <158798132+srikumartdba@users.noreply.github.com>
2024-04-15 21:40:55 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "example" {
+resource "aws_vpc" "project-vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
 output "vpc_id" {
-  value = aws_vpc.example.id
+  value = aws_vpc.project-vpc.id
 }
